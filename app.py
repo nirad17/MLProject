518e0e9008822dbb3dbdd29a1ce99ea4562ed44a
Nirad Yeola <yeola.nirad@gmail.com>
2023-06-19 13:33:10 +0530

    Predict Pipeline created

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, request, render_template
 
+from src.pipline.predict_pipeline import CustomData, PredictPipeline
+
 application = Flask(__name__)
 
 app= application
@@ -13,6 +15,24 @@ def predict_datapoint():
     if request.method=="GET":
         return render_template("home.html")
     else:
-        pass
+        data=CustomData(
+            gender=request.form.get('gender'),
+            race_ethnicity=request.form.get('race-ethnicity'),
+            parental_level_of_education=request.form.get('parental-education'),
+            lunch=request.form.get('lunch'),
+            test_preparation_course=request.form.get('test-preparation'),
+            reading_score=float(request.form.get('reading-score')),
+            writing_score=float(request.form.get('writing-score'))
+
+        )
+        pred_df=data.get_data_as_data_frame()
+        print(pred_df)
+        print("Before Prediction")
+
+        predict_pipeline=PredictPipeline()
+        print("Mid Prediction")
+        results=predict_pipeline.predict(pred_df)
+        print("after Prediction")
+        return render_template('home.html',results=results[0])
 if __name__=="__main__":
     app.run(host="0.0.0.0",debug=True)
